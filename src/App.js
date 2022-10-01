79edf2747a848608f17a579af96314f0af177f0b
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-10-01 18:27:24 -0300

    refactor: alterando o App.js. Incluindo rotas e aplicando global-styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Registration from './pages/Registration';
+import Users from './pages/Users';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/registration" component={ Registration } />
+          <Route path="/users" component={ Users } />
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
+
+// function Home() {
+//   return <h2>Home</h2>
+// }
 
 export default App
