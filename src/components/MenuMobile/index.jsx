151518b3a7a095f2b054c49564317aab63c6ef26
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-10-01 21:42:27 -0300

    refactor: alterado a tag <a> para <Link> do react-router-dom

--- src/components/MenuMobile/index.jsx
+++ src/components/MenuMobile/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Container, Menu, MenuList} from './styles'
 import menuHamburger from '../../assets/icons/hamburguer-aberto0.svg'
+import { Link } from 'react-router-dom'
 
 const MenuMobile = ({menuIsVisible, setMenuIsVisible}) => {
   return (
     <Container isVisible={menuIsVisible}>
       <Menu src={menuHamburger} onClick={() => setMenuIsVisible(false)}/>
       <MenuList>
-        <a href="#">lista</a>
-        <a href="#">sobre mim</a>
-        <a href="#">cadastro</a>
+          <Link to="/users">lista</Link>
+          <Link to="/">Sobre mim</Link>
+          <Link to="/registration">Cadastro</Link>
       </MenuList>
     </Container>
   )
