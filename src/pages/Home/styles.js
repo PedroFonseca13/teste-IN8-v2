b3ba10b71d775818d43c0ab07af1a30e443119d5
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-10-01 22:51:34 -0300

    style: adicionado estilo para breakpoint min-1024px

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,29 +1,49 @@
 import styled from 'styled-components'
 import backgroundMobile from '../../assets/img/index-image-mobile.jpg'
+import backgroundOriginal from '../../assets/img/index-image.jpg'
 
 export const BodyBackground = styled.body`
   background-image: url(${backgroundMobile});
   background-position: center;
   background-size: cover;
 
+  @media screen and (min-width: 1024px){
+    background-image: url(${backgroundOriginal});
+    margin: 0 auto;
+  }
 `
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  min-width: 100vw;
+  /* min-width: 100vw; */
   flex-direction: column;
   padding-top: 40px;
+
+  @media screen and (min-width:1024px){
+    align-items: flex-start;
+    margin-top: 190px;
+    margin-left: 160px;
+  }
 `;
 
 export const Title = styled.h1`
   text-transform: uppercase;
   font-size: 3.2rem;
   font-weight: 100;
+  color: #fff;
+
+  @media screen and (min-width:1024px){
+    font-size: 4rem;
+  }
 `;
 
 export const Text = styled.p`
   text-transform: uppercase;
   font-size: 1.4rem;
+  color: #fff;
+
+  @media screen and (min-width:1024px){
+    font-size: 1.7rem;
+  }
 `
