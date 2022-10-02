dcd6ffa9228971ce124824f522d21c421a866bbd
Pedro Fonseca <pedrinhopedro01@gmail.com>
2022-10-02 20:50:51 -0300

    style: formatacao do linter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react'
-import { Container, BodyBackground, Title, Text } from './styles'
-import Header from '../../components/Header'
-import GlobalStyle from '../../styles/global'
-import MenuMobile from '../../components/MenuMobile'
+import React, { useState } from 'react';
+import { Container, BodyBackground, Title, Text } from './styles';
+import Header from '../../components/Header';
+import GlobalStyle from '../../styles/global';
+import MenuMobile from '../../components/MenuMobile';
 
 const Home = () => {
-  const [menuIsVisible, setMenuIsVisible] = useState(false)
+  const [menuIsVisible, setMenuIsVisible] = useState(false);
   return (
     <>
       <GlobalStyle />
       <BodyBackground>
-      <MenuMobile
-        menuIsVisible={menuIsVisible}
-        setMenuIsVisible={setMenuIsVisible}
-      />
-      <Header
-        setMenuIsVisible={setMenuIsVisible}
-      />
-      <Container>
-        <Title>estágio</Title>
-        <Text>
-          prova de seleção
-        </Text>
-      </Container>
+        <MenuMobile
+          menuIsVisible={menuIsVisible}
+          setMenuIsVisible={setMenuIsVisible}
+        />
+        <Header setMenuIsVisible={setMenuIsVisible} />
+        <Container>
+          <Title>estágio</Title>
+          <Text>prova de seleção</Text>
+        </Container>
       </BodyBackground>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
